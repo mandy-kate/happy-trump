49610a5011b1e8031c16210351387dc6a279a0b8
mandy-kate <mandykatebaker@gmail.com>
2017-04-29 17:35:40 +1200

    serving tweets at random

--- server.js
+++ server.js
@@ -3,16 +3,6 @@ var path = require('path')
 var hbs = require('express-handlebars')
 var data = require('./index.js');
 var server = express()
-//var data = require('..happy-trump/datastore/db.json') //Placeholder
-// var data = {
-//   "people": [
-//     { "name": "Emily", "age": 56 },
-//     { "name": "Tama", "age": 31 },
-//     { "name": "Sarah", "age": 22 },
-//     { "name": "Jill", "age": 77 },
-//     { "name": "Fred", "age": 25 }
-//   ]
-// }
 
 var PORT = process.env.PORT || 3000
 
@@ -28,7 +18,7 @@ server.set('views', path.join(__dirname, './views'))
 server.use(express.static('public'))
 
 // routes
-server.get('/quotes', function (req, res) {
+server.get('/', function (req, res) {
   data.getDonaldTweets(5, function(tweets){
     res.render('index', {results: tweets, positive: data.positive})
   })
